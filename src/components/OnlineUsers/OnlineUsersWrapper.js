a5f2fc02e759a3bfd85fcdbebbe9710a68f657b1
sthtnr <beautifulwindmi11@gmail.com>
2019-08-29 20:47:18 +0900

    implement CRUD operation with gql's query and mutation

--- src/components/OnlineUsers/OnlineUsersWrapper.js
+++ src/components/OnlineUsers/OnlineUsersWrapper.js
@@ -1,28 +1,45 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { withApollo } from 'react-apollo';
 
-import OnlineUser from "./OnlineUser";
+import OnlineUser from './OnlineUser';
 
 class OnlineUsersWrapper extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.client = props.client;
 
     this.state = {
-      onlineUsers: [
-        { name: "someUser1" },
-        { name: "someUser2" }
-      ]
+      onlineUsers: [{ name: 'someUser1' }, { name: 'someUser2' }],
     };
+  }
+
+  updateLastSeen() {
+    // use the apollo client to run a mutation to update the last_seen value
+    const UPDATE_LASTSEEN_MUTATION = gql`
+      mutation updateLastSeen($now: timestamptz!) {
+        update_users(where: {}, _set: { last_seen: $now }) {
+          affected_rows
+        }
+      }
+    `;
+    this.client.mutate({
+      mutation: UPDATE_LASTSEEN_MUTATION,
+      variables: { now: new Date().toISOString() },
+    });
+  }
+
+  componentDidMount() {
+    // every 30s, run a mutation to tell the backend that you're online
+    this.onlineIndicator = setInterval(() => this.updateLastSeen(), 30000);
   }
 
   render() {
     const onlineUsersList = [];
     this.state.onlineUsers.forEach((user, index) => {
       onlineUsersList.push(
-        <OnlineUser
-          key={index}
-          index={index}
-          user={user}
-        />);
+        <OnlineUser key={index} index={index} user={user} />
+      );
     });
 
     return (
@@ -31,10 +48,10 @@ class OnlineUsersWrapper extends Component {
           Online users - {this.state.onlineUsers.length}
         </div>
 
-        { onlineUsersList }
+        {onlineUsersList}
       </div>
     );
   }
 }
 
-export default OnlineUsersWrapper;
+export default withApollo(OnlineUsersWrapper);
--- src/components/Todo/TodoPrivateList.js
+++ src/components/Todo/TodoPrivateList.js
@@ -1,73 +1,93 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment } from 'react';
 
-import TodoItem from "./TodoItem";
-import TodoFilters from "./TodoFilters";
+import TodoItem from './TodoItem';
+import TodoFilters from './TodoFilters';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const GET_MY_TODOS = gql`
+  query getMyTodos {
+    todos(
+      where: { is_public: { _eq: false } }
+      order_by: { created_at: desc }
+    ) {
+      id
+      title
+      created_at
+      is_completed
+    }
+  }
+`;
 
 class TodoPrivateList extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      filter: "all",
+      filter: 'all',
       clearInProgress: false,
-      todos: [
-        {
-          id: "1",
-          title: "This is private todo 1",
-          is_completed: true,
-          is_public: false
-        },
-        {
-          id: "2",
-          title: "This is private todo 2",
-          is_completed: false,
-          is_public: false
-        }
-      ]
     };
 
     this.filterResults = this.filterResults.bind(this);
     this.clearCompleted = this.clearCompleted.bind(this);
+
+    // set the apollo client
+    this.client = props.client;
   }
 
   filterResults(filter) {
     this.setState({
       ...this.state,
-      filter: filter
+      filter: filter,
     });
   }
 
-  clearCompleted() {}
+  clearCompleted() {
+    // remove all the todos that are completed
+    const CLEAR_COMPLETED = gql`
+      mutation clearCompleted {
+        delete_todos(
+          where: { is_completed: { _eq: true }, is_public: { _eq: false } }
+        ) {
+          affected_rows
+        }
+      }
+    `;
+
+    this.client.mutate({
+      mutation: CLEAR_COMPLETED,
+      optimisticResponse: {},
+      update: (cache, { data }) => {
+        const existingTodos = cache.readQuery({ query: GET_MY_TODOS });
+        const newTodos = existingTodos.todos.filter(t => !t.is_completed);
+        cache.writeQuery({ query: GET_MY_TODOS, data: { todos: newTodos } });
+      },
+    });
+  }
 
   render() {
-    let filteredTodos = this.state.todos;
-    if (this.state.filter === "active") {
-      filteredTodos = this.state.todos.filter(todo => todo.is_completed !== true);
-    } else if (this.state.filter === "completed") {
-      filteredTodos = this.state.todos.filter(todo => todo.is_completed === true);
+    const { todos } = this.props;
+
+    let filterdTodos = todos;
+    if (this.state.filter === 'active') {
+      filterdTodos = todos.filter(todo => todo.is_completed !== true);
+    } else if (this.state.filter === 'completed') {
+      filterdTodos = todos.filter(todo => todo.is_completed === true);
     }
 
     const todoList = [];
-    filteredTodos.forEach((todo, index) => {
-      todoList.push(
-        <TodoItem
-          key={index}
-          index={index}
-          todo={todo}
-        />
-      );
+    filterdTodos.forEach((todo, index) => {
+      todoList.push(<TodoItem key={index} index={index} todo={todo} />);
     });
 
     return (
       <Fragment>
         <div className="todoListWrapper">
-          <ul>
-            { todoList }
-          </ul>
+          <ul>{todoList}</ul>
         </div>
 
         <TodoFilters
-          todos={filteredTodos}
+          todos={filterdTodos}
           currentFilter={this.state.filter}
           filterResultsFn={this.filterResults}
           clearCompletedFn={this.clearCompleted}
@@ -78,4 +98,22 @@ class TodoPrivateList extends Component {
   }
 }
 
-export default TodoPrivateList;
+const TodoPrivateListQuery = () => {
+  return (
+    <Query query={GET_MY_TODOS}>
+      {({ loading, error, data, client }) => {
+        if (loading) {
+          return <div>Loading...</div>;
+        }
+        if (error) {
+          console.error(error);
+          return <div>Error!</div>;
+        }
+        return <TodoPrivateList client={client} todos={data.todos} />;
+      }}
+    </Query>
+  );
+};
+
+export default TodoPrivateListQuery;
+export { GET_MY_TODOS };
